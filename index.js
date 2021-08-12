183b4416141b2a977c312f4efe8fc937bc404de3
Sanam <sanampakuwal27@gmail.com>
2021-08-13 00:09:00 +0545

    score feature added and js refactor

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const cards = document.querySelectorAll(".card");
-console.log(cards);
 
-// variable
-let isFlipped = false;
-let firstCard;
-let secondCard;
+let [isFlipped, firstCard, secondCard, currentScore, totalScore] = [
+  false,
+  null,
+  null,
+  0,
+  8,
+];
 
 cards.forEach((card) => card.addEventListener("click", flip));
 
 function flip() {
-  // console.log("Card flipped");
-  // console.log(this);
   this.classList.add("flip");
   if (!isFlipped) {
     isFlipped = true;
@@ -21,49 +21,49 @@ function flip() {
   }
 }
 
-function checkIt() {
-  // console.log("Checking...");
+let checkIt = async () => {
   if (firstCard.dataset.image === secondCard.dataset.image) {
     success();
   } else {
     fail();
   }
-}
+};
 
-function success() {
-  // console.log("Success!");
+let success = async () => {
   firstCard.removeEventListener("click", flip);
   secondCard.removeEventListener("click", flip);
+  currentScore += 1;
+  document.querySelector(
+    ".score"
+  ).innerText = `Score : ${currentScore.toString()} in 8 🎉`;
+  if (currentScore === totalScore) {
+    document.querySelector(".score").innerText = `Congratulations 🎉🎉`;
+  }
   reset();
-}
+};
 
-function fail() {
-  // console.log("Fail!");
+let fail = async () => {
   setTimeout(() => {
     firstCard.classList.remove("flip");
     secondCard.classList.remove("flip");
     reset();
   }, 500);
-}
+};
 
-function reset() {
+let reset = async () => {
   isFlipped = false;
   firstCard = null;
   secondCard = null;
-}
+};
 
-// flexbox type
-function shuffle() {
+let shuffle = async () => {
   cards.forEach((card) => {
     let index = Math.floor(Math.random() * 16);
     card.style.order = index;
   });
-}
+};
 
-shuffle();
-
-//TODO:
-// make all arrow function
-// destructering
-// shuffle function make windows.load
-// fix bugs
+// shuffle();
+window.addEventListener("load", () => {
+  shuffle();
+});
